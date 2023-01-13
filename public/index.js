4ec3ec71d7fd52c63340ef5da83029f1ec333541
Levis Sikudi <levisikudi@gmail.com>
2023-01-13 10:49:05 -0500

    list of button pokemon functionality made

--- public/index.js
+++ public/index.js
@@ -1,1 +1,20 @@
-console.log("Working");
+let button = document.querySelector('button')
+let body = document.querySelector('body')
+let ol = document.createElement('ol')
+body.appendChild(ol)
+
+button.addEventListener('click', async () =>{
+   let response = await fetch('http://localhost:4000/get_pokemon_data')
+   let parsedData = await response.json()
+   
+    
+    parsedData.forEach(data => {
+        // let the data sit in an li
+        let li = document.createElement('li')
+        li.textContent = data.name
+        ol.appendChild(li)
+        
+    });
+
+    
+})
--- server.js
+++ server.js
@@ -5,8 +5,18 @@ const app = express()
 app.use(express.static('public'));
 
 
-app.get('/', (req, res)=>{
-    res.send("Code is working")
+const pokemon = [
+    {name: "bulbasaur", img: "http://img.pokemondb.net/artwork/bulbasaur", age: 3},
+    {name: "ivysaur", img: "http://img.pokemondb.net/artwork/ivysaur", age: 4},
+    {name: "venusaur", img: "http://img.pokemondb.net/artwork/venusaur", age: 5},
+    {name: "charmander", img: "http://img.pokemondb.net/artwork/charmander", age: 2},
+    {name: "charizard", img: "http://img.pokemondb.net/artwork/charizard", age: 7},
+    {name: "squirtle", img: "http://img.pokemondb.net/artwork/squirtle", age: 8},
+    {name: "wartortle", img: "http://img.pokemondb.net/artwork/wartortle", age: 6}
+];
+
+app.get('/get_pokemon_data', (req, res)=>{
+    res.send(pokemon)
 })
 
 
